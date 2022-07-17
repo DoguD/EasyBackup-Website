c16bf7bd74be79416711e893c4936358515f6980
DoguD <dogudeniz.ugur@gmail.com>
2022-07-18 02:54:03 +0300

    weekly update

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ let discountedContractWithSigner;
 let signer;
 
 export default function Home() {
-    const giveAwayCutOffCount = 648;
+    const giveAwayCutOffCount = 702;
     const giveAwayRewardCount = 2;
     const [walletAddress, setWalletAddress] = useState("");
     const [minted, setMinted] = useState(0);
