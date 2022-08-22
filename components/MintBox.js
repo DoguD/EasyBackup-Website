40f7379faf55565febc08a4ed7f61c6e20b6b463
DoguD <dogudeniz.ugur@gmail.com>
2022-08-23 02:30:34 +0300

    generation 2 update

--- components/MintBox.js
+++ components/MintBox.js
@@ -12,11 +12,14 @@ export default function MintBox(props) {
             <p className={styles.generationText} style={{textDecoration: "line-through"}}>
                 <b>Generation 0:</b> Sold-out
             </p>
+            <p className={styles.generationText} style={{textDecoration: "line-through"}}>
+                <b>Generation 1:</b> Sold-out
+            </p>
             <p className={styles.generationText}>
                 <b>Generation {Math.floor(props.minted / 1000) + 1} left:</b>
-                {props.minted === 0 ? "900" :
+                {props.minted === 0 ? "1000" :
                 1000 - props.minted % 1000 }
-                / 900
+                / 1000
             </p>
             <div style={{height: 16}}/>
             <ProgressBar bgColor={"#3a70ed"}
