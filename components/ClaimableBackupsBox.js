1edc289526797df8f7d308e650b1318ac4c9fbee
DoguD <dogudeniz.ugur@gmail.com>
2022-12-28 18:32:07 +0300

    claimable backups basics done

--- components/ClaimableBackupsBox.js
+++ components/ClaimableBackupsBox.js
@@ -1,27 +1,33 @@
 import styles from "../styles/Home.module.css";
+import {useState} from "react";
+import {Button} from 'semantic-ui-react'
 
 export default function ClaimableBackupsBox(props) {
+    const [isClaiming, setIsClaiming] = useState(false);
     return (
         <>
             <h2 className={styles.subTitle}>
-                Stats
+                Claimable Backups
             </h2>
-            <div className={styles.grid}>
-                <div className={styles.card}>
-                    <h2>Total Backed-Up Assets</h2>
-                    <p>$1,000,000</p>
-                </div>
-                <div className={styles.card}>
-                    <h2>$EASY Token Value</h2>
-                    <p>$0.01</p>
-                </div>
-                <div className={styles.card}>
-                    <h2>$EASY Market Cap</h2>
-                    <p>$50,000</p>
-                </div>
-                <div className={styles.card}>
-                    <h2>Total Protocol Revenue</h2>
-                    <p>$100</p>
+            <div className={styles.claimableBackupsContainer}>
+                <div className={styles.claimableBackupsRow}>
+                    <p className={styles.claimableBackupText}><b>From: </b></p>
+                    <p className={styles.claimableBackupText}>0x14453a83131012F4DbB4c9c98830A0DE04B38c10</p>
+
+                    <div style={{width: 16}}/>
+                    <p className={styles.claimableBackupText}><b>Token: </b></p>
+                    <p className={styles.claimableBackupText}>$ETH</p>
+
+                    <div style={{width: 16}}/>
+                    <p className={styles.claimableBackupText}><b>Amount: </b></p>
+                    <p className={styles.claimableBackupText}>0.1463</p>
+
+                    <div style={{width: 16}}/>
+                    <p className={styles.claimableBackupText}><b>Can Be Claimed In: </b></p>
+                    <p className={styles.claimableBackupText}>12 days</p>
+
+                    <div style={{width: 32}}/>
+                    <Button primary disabled>Claim</Button>
                 </div>
             </div>
         </>
