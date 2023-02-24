2d6a402067aa7a272c30facf59cd728b59af2df0
DoguD <dogudeniz.ugur@gmail.com>
2023-02-24 19:50:43 +0800

    Buy easy temporarily disabled

--- components/subComponents/IconContainer.js
+++ components/subComponents/IconContainer.js
@@ -6,9 +6,11 @@ import {EASY_ADDRESS} from "../../contracts/EasyToken";
 export default function IconContainer(props) {
     return (
         <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+            {/* TODO:
             <p className={styles.getTokenText} style={{margin: 0}}
                onClick={() => window.open("https://spooky.fi/#/swap?inputCurrency=0x04068DA6C83AFCFA0e13ba15A6696662335D5B75&outputCurrency=" + EASY_ADDRESS, "_blank")}>Buy
                 $EASY</p>
+                */}
             <AiOutlineTwitter color={"#3f8bd2"} size={32} className={styles.navbarIcon}
                               onClick={() => window.open("https://twitter.com/easyblock_fin", "_blank")}/>
             <FaDiscord color={"#5568e3"} size={32} className={styles.navbarIcon}
