ae14e7052860c478a12c729f7ecc660efaf5c37b
DoguD <dogudeniz.ugur@gmail.com>
2023-03-12 23:14:31 +0800

    docs logo added

--- components/subComponents/IconContainer.js
+++ components/subComponents/IconContainer.js
@@ -1,7 +1,8 @@
 import styles from "../../styles/Home.module.css";
-import {AiFillMediumSquare, AiOutlineTwitter} from "react-icons/ai";
+import {AiFillMediumSquare, AiOutlineTwitter, AiOutlineBook} from "react-icons/ai";
 import {FaDiscord} from "react-icons/fa";
 import {EASY_ADDRESS} from "../../contracts/InProduction/EasyToken";
+import {GiWhiteBook, GiBlackBook} from "react-icons/gi";
 
 export default function IconContainer(props) {
     return (
@@ -9,6 +10,8 @@ export default function IconContainer(props) {
             <p className={styles.getTokenText} style={{margin: 0}}
                onClick={() => window.open("https://spooky.fi/#/swap?inputCurrency=0x04068DA6C83AFCFA0e13ba15A6696662335D5B75&outputCurrency=" + EASY_ADDRESS, "_blank")}>Buy
                 $EASY</p>
+            <GiWhiteBook color={"#3a70ed"} size={26} className={styles.navbarIcon}
+                           onClick={() => window.open("https://docs.backup.easyblock.finance", "_blank")}/>
             <AiOutlineTwitter color={"#3f8bd2"} size={32} className={styles.navbarIcon}
                               onClick={() => window.open("https://twitter.com/easyblock_fin", "_blank")}/>
             <FaDiscord color={"#5568e3"} size={32} className={styles.navbarIcon}
